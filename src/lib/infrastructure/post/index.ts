09c343060ff20144c3a0712eb5a7a858655574d0
rust1cean <az.az.az.az.75007500@gmail.com>
2025-01-04 18:06:27 +0500

    Changes

--- src/lib/infrastructure/post/index.ts
+++ src/lib/infrastructure/post/index.ts
@@ -2,6 +2,8 @@ import type { Merge } from 'type-fest';
 import type { Tables } from '../db';
 
 export * from './remote-post-repository-impl';
+export * from './in-memory-post-repository-impl';
+export * from './memory-first-post-repository-impl';
 
 export const POSTS_PER_REQUEST_LIMIT: number = 40;
 
---
+++ src/lib/infrastructure/post/memory-first-post-repository-impl.ts
@@ -0,0 +1,45 @@
+import type { CreatePostData, FetchPostsOptions, PostRepository, UpdatePostData } from '$lib/application/post';
+import type { PostId } from '$lib/domain/common/repository';
+import type { PostEntity } from '$lib/domain/post';
+
+export class MemoryFirstPostRepositoryImpl implements PostRepository {
+	constructor(
+		private inMemoryRepository: PostRepository,
+		private remoteRepository: PostRepository
+	) {}
+
+	public async fetchPosts(options: FetchPostsOptions): Promise<PostEntity[]> {
+    let posts = await this.inMemoryRepository.fetchPosts(options);
+
+    if (posts.length === 0) {
+      posts = await this.remoteRepository.fetchPosts(options);
+    }
+
+    return posts;
+  }
+
+  public async fetchPostById(postId: PostId): Promise<PostEntity | null> {
+    let post = await this.inMemoryRepository.fetchPostById(postId)
+
+    if (!post) {
+      post = await this.remoteRepository.fetchPostById(postId)
+    }
+
+    return post;
+  }
+
+  public async createPost(postData: CreatePostData): Promise<void> {
+    this.inMemoryRepository.createPost(postData)
+    this.remoteRepository.createPost(postData)
+  }
+
+  public async updatePost(postId: PostId, postData: UpdatePostData): Promise<void> {
+    this.inMemoryRepository.updatePost(postId, postData)
+    this.remoteRepository.updatePost(postId, postData)
+  }
+
+  public async deletePost(postId: PostId): Promise<void> {
+    this.inMemoryRepository.deletePost(postId)
+    this.remoteRepository.deletePost(postId)
+  }
+}
